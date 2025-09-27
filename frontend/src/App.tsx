9c4b0be914521f3e2a59d86d5c48abb36c4766b7
Hoo Di Heng <Hoodiheng@gmail.com>
2025-09-27 22:37:27 +0800

    Edit vercel json

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./assets/styles/app.css";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
 	/*
@@ -13,7 +14,7 @@ const App = () => {
 			<Routes>
 				<Route index element={<Home />}></Route>
 				{/*Missed routes*/}
-				<Route path="*" />
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
 	);
