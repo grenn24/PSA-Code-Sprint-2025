9bf7b377f4bd366e3f4056b65c6409846dd3c1c0
Hoo Di Heng <Hoodiheng@gmail.com>
2025-09-29 00:57:22 +0800

    Add new origin

--- frontend/src/utilities/apiClient.ts
+++ frontend/src/utilities/apiClient.ts
@@ -20,6 +20,7 @@ export class ApiClient {
 			headers: {
 				"Content-Type": "application/json", // dont include x-access-token as default headers (since it will change frequently)
 				Accept: "application/json",
+				Origin: "https://psa-code-sprint-2025.vercel.app",
 			},
 		});
 		client.interceptors.request.use(
